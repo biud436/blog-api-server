53e437e1d76945063f9405920c8b1211d64e3a59
biud436 <biud436@gmail.com>
2022-03-01 22:39:09 +0900

    포스트 디자인 변경

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,19 @@
 # Introduction
 
 프론트엔드 페이지를 만들기 위한 프로젝트입니다. 목표는 서버 사이드 렌더링 프레임워크를 이용하여 최종적으로 정적 페이지를 생성해야 합니다.
+
+## 설치 방법
+
+before starting the fronent development, you need to install the following dependencies:
+
+```
+yarn install
+```
+
+## 개발 서버 실행 방법
+
+To start the develoment server, run the following command:
+
+```bash
+yarn dev
+```
--- frontend/pages/posts/[id].tsx
+++ frontend/pages/posts/[id].tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
 import { GetStaticProps, GetStaticPaths } from "next";
+import Link from "next/link";
 
 type PostDataProp = {
     postData: {
@@ -19,15 +20,48 @@ export default function Post({ postData }: PostDataProp) {
             <Head>
                 <title>{postData.title}</title>
             </Head>
-            <article>
-                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-                <div className={utilStyles.lightText}>
+            <article className="rounded border shadow p-2 m-2">
+                <h1
+                    className={`${utilStyles.headingXl} hover:underline cursor-pointer`}
+                >
+                    {postData.title}
+                </h1>
+                <div className={`${utilStyles.lightText} hover:underline`}>
                     <Date dateString={postData.date} />
                 </div>
                 <div
+                    className="text-sm border rounded p-2 mt-4"
                     dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
                 />
             </article>
+            <section className="p-2 m-2 bg-gray-100 rounded border">
+                <div className="text-left">
+                    <div>
+                        댓글 <span className="text-red-600 px-1">1</span>개{" "}
+                    </div>
+                    <div className="p-2 mt-4">
+                        <div className="border rounded p-2 m-1 w-32 bg-[rgba(255,255,255,0.5)]">
+                            댓글 작성자
+                        </div>
+                        <div className="border rounded p-2 m-1 bg-[rgba(255,255,255,0.5)]">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit.
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-2 text-right">
+                    <span className="hover:underline cursor-pointer border rounded p-2 bg-[rgba(255,255,255,0.5)]">
+                        댓글 쓰기
+                    </span>
+                </div>
+            </section>
+            <section className="text-right m-2 p-2">
+                <Link href="/">
+                    <a className="hover:underline border rounded p-2 w-32 text-black">
+                        포스트 목록 보기
+                    </a>
+                </Link>
+            </section>
         </Layout>
     );
 }
