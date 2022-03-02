0c490d951a500adf63ad99e2f77747746daeabd6
biud436 <biud436@gmail.com>
2022-03-02 11:07:40 +0900

    rewrites에서 rewrite로 변경

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,19 +6,19 @@ const withTM = require("next-transpile-modules");
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${process.env.NEXT_PUBLIC_SERVER_IP}/:path*`, // Proxy to Backend
-            },
-        ];
-    },
-    reactStrictMode: true,
-    webpack: (config) => {
-        config.plugins.push(new Dotenv({ silent: true }));
-        return config;
-    },
+  async rewrite() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_SERVER_IP}/:path*`, // Proxy to Backend
+      },
+    ];
+  },
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({ silent: true }));
+    return config;
+  },
 };
 
 module.exports = nextConfig;
