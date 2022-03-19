376f0e449af11acd97da0ca10e34288b96149d2b
biud436 <biud436@gmail.com>
2022-03-19 16:59:12 +0900

    #4 / AWS로 이메일 발송, config 변수 정리

--- backend/src/__spec__/email-aws.spec.ts
+++ backend/src/__spec__/email-aws.spec.ts
@@ -17,21 +17,8 @@ class EmailService {
       });
     });
   }
-}
 
-describe('이메일 전송', () => {
-  it('AWS SES로 메일 발송', async () => {
-    const value = dotenv.config({ path: 'aws.env' });
-
-    const client = new AWS.SES({
-      region: 'ap-northeast-2',
-      credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY,
-      },
-      apiVersion: '2012-10-17',
-    });
-
+  get options(): AWS.SES.SendEmailRequest {
     // 한글 처리
     const base64ToName = Buffer.from(`관리자`).toString('base64');
     const finalToName = `=?UTF-8?B?${base64ToName}?= <${process.env.TO_EMAIL_ADDRESS}>`;
@@ -47,12 +34,12 @@ describe('이메일 전송', () => {
           Html: {
             Charset: 'UTF-8',
             Data: `<html>
-                  <head></head>
-                  <body>
-                    <h1>안녕하세요</h1>
-                    <p>테스트 이메일을 보냅니다.</p>
-                  </body>
-                  </html>`,
+                        <head></head>
+                        <body>
+                          <h1>안녕하세요</h1>
+                          <p>테스트 이메일을 보냅니다.</p>
+                        </body>
+                        </html>`,
           },
           Text: {
             Charset: 'UTF-8',
@@ -67,7 +54,28 @@ describe('이메일 전송', () => {
       Source: finalToName /* required */,
     };
 
-    const result = await Container.get(EmailService).sendMail(client, config);
+    return config;
+  }
+}
+
+describe('이메일 전송', () => {
+  const emailService: EmailService = Container.get(EmailService);
+
+  it('AWS SES로 메일 발송', async () => {
+    const value = dotenv.config({ path: 'aws.env' });
+
+    const client = new AWS.SES({
+      region: 'ap-northeast-2',
+      credentials: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+      },
+      apiVersion: '2012-10-17',
+    });
+
+    const config = emailService.options;
+
+    const result = await emailService.sendMail(client, config);
     if (result) {
       expect(result).toBeTruthy();
     }
