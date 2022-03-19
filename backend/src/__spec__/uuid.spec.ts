9c06a33a14db8ca194be6008dcc5f695ceb6a2d7
biud436 <biud436@gmail.com>
2022-03-19 16:33:36 +0900

    added AES_256_KEY and AES_256_IV properties to configuration files

--- backend/src/__spec__/uuid.spec.ts
+++ backend/src/__spec__/uuid.spec.ts
@@ -8,12 +8,21 @@ describe('create uuid v4', () => {
     logger = new Logger('test');
   });
 
-  it('should create a uuid v4', () => {
+  it('1', () => {
     const value: string = uuidv4();
     const normalizeUUID = value.replace(/[\-]+/g, '');
 
     logger.log(normalizeUUID); // 3d9eddd73e2647e894c4dc8091416312
-    logger.log(normalizeUUID.length); // 32 bytes
+
+    expect(value).toMatch(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+    );
+  });
+  it('2', () => {
+    const value: string = uuidv4();
+    const normalizeUUID = value.replace(/[\-]+/g, '');
+
+    logger.log(normalizeUUID); // 3d9eddd73e2647e894c4dc8091416312
 
     expect(value).toMatch(
       /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
