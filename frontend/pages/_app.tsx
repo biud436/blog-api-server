14e3933d7aafa47c452c04769f001e70312cb0ad
biud436 <biud436@gmail.com>
2022-04-25 23:01:02 +0900

    fix(): 타입 제거

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,10 +2,10 @@ import "../styles/global.css";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  );
+export default function App({ Component, pageProps }) {
+    return (
+        <RecoilRoot>
+            <Component {...pageProps}></Component>
+        </RecoilRoot>
+    );
 }
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -2,13 +2,13 @@ import Layout from "../components/layout";
 import LoginForm from "../components/tools/login-form";
 
 export default function Login() {
-  return (
-    <Layout>
-      <div className="container-xl flex justify-center items-center">
-        <div className="w-96">
-          <LoginForm></LoginForm>
-        </div>
-      </div>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <div className="container-xl flex justify-center items-center">
+                <div className="w-96">
+                    <LoginForm></LoginForm>
+                </div>
+            </div>
+        </Layout>
+    );
 }
