29b6f813ed1eaa431fe36a700ebb20a7ead7b211
biud436 <biud436@gmail.com>
2022-04-25 23:09:42 +0900

    feat: material-ui 추가

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "../styles/global.css";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function App({ Component, pageProps }) {
     return (
         <RecoilRoot>
+            <CssBaseline />
             <Component {...pageProps}></Component>
         </RecoilRoot>
     );
