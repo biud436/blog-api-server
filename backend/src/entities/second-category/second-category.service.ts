1a9079eb261ea8580bc50a0220d4a8fc706da782
biud436 <biud436@gmail.com>
2022-05-03 23:56:29 +0900

    feat: `InjectRepository` 데코레이터 추가

--- backend/src/entities/second-category/second-category.service.ts
+++ backend/src/entities/second-category/second-category.service.ts
@@ -1,6 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateSecondCategoryDto } from './dto/create-second-category.dto';
 import { UpdateSecondCategoryDto } from './dto/update-second-category.dto';
+import { SecondCategory } from './entities/second-category.entity';
+import { SecondCategoryRepository } from './entities/second-category.repository';
 
 @Injectable()
-export class SecondCategoryService {}
+export class SecondCategoryService {
+    constructor(
+        @InjectRepository(SecondCategory)
+        private readonly firstCategoryRepository: SecondCategoryRepository,
+    ) {}
+}
