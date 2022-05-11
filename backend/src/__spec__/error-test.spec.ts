de14924d44fbce5f10a8c1d115fff93e2f15ee71
biud436 <biud436@gmail.com>
2022-05-12 00:36:45 +0900

    fix: errorInterceptor 테스트 코드 작성

--- backend/src/__spec__/error-test.spec.ts
+++ backend/src/__spec__/error-test.spec.ts
@@ -3,65 +3,45 @@ class StageError extends Error {}
 class ProgramError extends Error {}
 class YoutubeError extends Error {}
 
+function errorInterceptor<T extends Error = Error>(e: T) {
+    if (e instanceof RaiseConditionError) {
+        console.log('RaiseConditionError');
+    } else if (e instanceof StageError) {
+        console.log('StageError');
+    } else if (e instanceof ProgramError) {
+        console.log('ProgramError');
+    } else {
+        console.log(e.message);
+    }
+}
+
 describe('오류 테스트', () => {
     it('RaiseConditionError', () => {
         try {
             throw new RaiseConditionError('오류 발생');
         } catch (e: any) {
-            if (e instanceof RaiseConditionError) {
-                console.log('RaiseConditionError');
-            } else if (e instanceof StageError) {
-                console.log('StageError');
-            } else if (e instanceof ProgramError) {
-                console.log('ProgramError');
-            } else {
-                console.log(e.message);
-            }
+            errorInterceptor(e);
         }
     });
     it('StageError', () => {
         try {
             throw new StageError('오류 발생');
         } catch (e: any) {
-            if (e instanceof RaiseConditionError) {
-                console.log('RaiseConditionError');
-            } else if (e instanceof StageError) {
-                console.log('StageError');
-            } else if (e instanceof ProgramError) {
-                console.log('ProgramError');
-            } else {
-                console.log(e.message);
-            }
+            errorInterceptor(e);
         }
     });
     it('ProgramError', () => {
         try {
             throw new ProgramError('오류 발생');
         } catch (e: any) {
-            if (e instanceof RaiseConditionError) {
-                console.log('RaiseConditionError');
-            } else if (e instanceof StageError) {
-                console.log('StageError');
-            } else if (e instanceof ProgramError) {
-                console.log('ProgramError');
-            } else {
-                console.log(e.message);
-            }
+            errorInterceptor(e);
         }
     });
     it('OtherError', () => {
         try {
             throw new YoutubeError('오류 발생');
         } catch (e: any) {
-            if (e instanceof RaiseConditionError) {
-                console.log('RaiseConditionError');
-            } else if (e instanceof StageError) {
-                console.log('StageError');
-            } else if (e instanceof ProgramError) {
-                console.log('ProgramError');
-            } else {
-                console.log(e.message);
-            }
+            errorInterceptor(e);
         }
     });
 });
