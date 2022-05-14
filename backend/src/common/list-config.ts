3b9736f22d12c1ca6e58e82282ceacb4e5e6d370
biud436 <biud436@gmail.com>
2022-05-14 18:00:07 +0900

    feat: list-config에 타입 추가

--- backend/src/common/list-config.ts
+++ backend/src/common/list-config.ts
@@ -2,10 +2,22 @@ import { BadRequestException, HttpCode, HttpException } from '@nestjs/common';
 import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 import * as validator from 'class-validator';
 
+export namespace Pagination {
+    export type Config = {
+        limit: {
+            condition: {
+                min: number;
+            };
+            max: number;
+            min: number;
+        };
+    };
+}
+
 /**
  * 페이지네이션 설정
  */
-export const PaginationConfig = {
+export const PaginationConfig = <Pagination.Config>{
     limit: {
         /**
          * 조건문 체크
@@ -27,17 +39,18 @@ export const PaginationConfig = {
 /**
  * 검색 옵션
  */
-export const SearchOption = {
-    /**
-     * 검색어를 처리합니다.
-     *
-     * @param query
-     * @returns
-     */
-    handleQuery: (query: string) => {
-        if (!validator.isString(query) || query.length === 0) {
-            throw new BadRequestException('검색어를 입력하세요.');
-        }
-        return decodeURIComponent(query);
-    },
-};
+export const SearchOption: { handleQuery: (query: string) => never | string } =
+    {
+        /**
+         * 검색어를 처리합니다.
+         *
+         * @param query
+         * @returns
+         */
+        handleQuery: (query: string) => {
+            if (!validator.isString(query) || query.length === 0) {
+                throw new BadRequestException('검색어를 입력하세요.');
+            }
+            return decodeURIComponent(query);
+        },
+    };
