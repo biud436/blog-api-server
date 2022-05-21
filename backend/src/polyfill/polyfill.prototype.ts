eaccce1043023a7ec9285ed26ee0138ec2cdf565
biud436 <biud436@gmail.com>
2022-05-21 19:40:11 +0900

    fix: removed custom getMany function to queryBuilder

--- backend/src/polyfill/polyfill.prototype.ts
+++ backend/src/polyfill/polyfill.prototype.ts
@@ -9,10 +9,10 @@ import { QueryExpressionMap } from 'typeorm/query-builder/QueryExpressionMap';
 
 declare module 'typeorm' {
     interface SelectQueryBuilder<Entity> {
-        getMany(
-            this: SelectQueryBuilder<Entity>,
-        ): Promise<Entity[] | undefined>;
-        getOne(this: SelectQueryBuilder<Entity>): Promise<Entity | undefined>;
+        // getMany(
+        //     this: SelectQueryBuilder<Entity>,
+        // ): Promise<Entity[] | undefined>;
+        // getOne(this: SelectQueryBuilder<Entity>): Promise<Entity | undefined>;
 
         /**
          * 페이지네이션 설정입니다.
@@ -46,33 +46,33 @@ declare module 'typeorm' {
     }
 }
 
-SelectQueryBuilder.prototype.getMany = async function () {
-    const { entities, raw } = await this.getRawAndEntities();
+// SelectQueryBuilder.prototype.getMany = async function () {
+//     const { entities, raw } = await this.getRawAndEntities();
 
-    const items = entities.map((entitiy, index) => {
-        const metaInfo = Reflect.getMetadata(VIRTUAL_COLUMN_KEY, entitiy) ?? {};
-        const item = raw[index];
+//     const items = entities.map((entitiy, index) => {
+//         const metaInfo = Reflect.getMetadata(VIRTUAL_COLUMN_KEY, entitiy) ?? {};
+//         const item = raw[index];
 
-        for (const [propertyKey, name] of Object.entries<string>(metaInfo)) {
-            entitiy[propertyKey] = item[name];
-        }
+//         for (const [propertyKey, name] of Object.entries<string>(metaInfo)) {
+//             entitiy[propertyKey] = item[name];
+//         }
 
-        return entitiy;
-    });
+//         return entitiy;
+//     });
 
-    return [...items];
-};
+//     return [...items];
+// };
 
-SelectQueryBuilder.prototype.getOne = async function () {
-    const { entities, raw } = await this.getRawAndEntities();
-    const metaInfo = Reflect.getMetadata(VIRTUAL_COLUMN_KEY, entities[0]) ?? {};
+// SelectQueryBuilder.prototype.getOne = async function () {
+//     const { entities, raw } = await this.getRawAndEntities();
+//     const metaInfo = Reflect.getMetadata(VIRTUAL_COLUMN_KEY, entities[0]) ?? {};
 
-    for (const [propertyKey, name] of Object.entries<string>(metaInfo)) {
-        entities[0][propertyKey] = raw[0][name];
-    }
+//     for (const [propertyKey, name] of Object.entries<string>(metaInfo)) {
+//         entities[0][propertyKey] = raw[0][name];
+//     }
 
-    return entities[0];
-};
+//     return entities[0];
+// };
 
 SelectQueryBuilder.prototype.setPagination = function (
     this: SelectQueryBuilder<any>,
