9155f1fb3033182ddb32936a30749c20a960a13a
biud436 <biud436@gmail.com>
2022-05-28 21:59:33 +0900

    refactor: 사용하지 않는 변수 주석 및 변수 제거

--- backend/src/domain/auth/auth.controller.ts
+++ backend/src/domain/auth/auth.controller.ts
@@ -32,10 +32,7 @@ import { DocsMapper } from 'src/common/swagger-config';
 export class AuthController {
     private logger: Logger = new Logger(AuthController.name);
 
-    constructor(
-        private readonly authService: AuthService,
-        private readonly configService: ConfigService,
-    ) {}
+    constructor(private readonly authService: AuthService) {}
 
     @Post('/login')
     @UseGuards(new LocalAuthGuard())
@@ -50,9 +47,6 @@ export class AuthController {
         try {
             const token = await this.authService.login(req.user);
 
-            // NestJS는 response를 직접 매핑하여 반환하는데, @Res 데코레이터를 사용하면 직접 send를 해야 한다.
-            // 그러나 쿠키나 헤더 등 일부만 직접 처리하고,
-            // 나머지는 그대로 NestJS에 맡겨 send 하려면 @Res({ passthrough: true })로 설정해야 응답을 제대로 보낼 수 있다.
             res.cookie('access_token', token.accessToken);
 
             return ResponseUtil.success(RESPONSE_MESSAGE.LOGIN_SUCCESS, {
