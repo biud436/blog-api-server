58e20b442cb904921b052841e8bc1449c3c7cb99
biud436 <biud436@gmail.com>
2022-05-28 21:59:12 +0900

    refactor: unused configService 제거

--- backend/src/domain/auth/guards/roles.guard.ts
+++ backend/src/domain/auth/guards/roles.guard.ts
@@ -1,9 +1,10 @@
 import {
-  CanActivate,
-  ExecutionContext,
-  forwardRef,
-  Inject,
-  Injectable,
+    CanActivate,
+    ExecutionContext,
+    forwardRef,
+    Inject,
+    Injectable,
+    Logger,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Reflector } from '@nestjs/core';
@@ -13,49 +14,50 @@ import { AuthService } from '../auth.service';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(
-    private reflector: Reflector,
-    private configService: ConfigService,
-    @Inject(forwardRef(() => AuthService)) private authService: AuthService,
-  ) {}
+    private logger: Logger = new Logger(RolesGuard.name);
 
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
-      context.getHandler(),
-      context.getClass(),
-    ]);
-    if (!requiredRoles) {
-      return true;
+    constructor(
+        private reflector: Reflector,
+        @Inject(forwardRef(() => AuthService)) private authService: AuthService,
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const requiredRoles = this.reflector.getAllAndOverride<Role[]>(
+            ROLES_KEY,
+            [context.getHandler(), context.getClass()],
+        );
+        if (!requiredRoles) {
+            return true;
+        }
+        const req = context.switchToHttp().getRequest() as Request;
+        const authorization = req.headers['authorization'];
+        if (!authorization) {
+            return false;
+        }
+
+        if (!authorization.startsWith('Bearer ')) {
+            this.logger.log('Bearer로 시작하지 않는 토큰입니다');
+            return false;
+        }
+
+        // 토큰을 추출합니다.
+        const token = authorization.slice(7, authorization.length);
+
+        // 특정 API를 제외합니다.
+        const isIgnoreJwt = Reflect.getOwnMetadata(
+            'ignoreJwt',
+            context.getHandler(),
+        );
+
+        if (isIgnoreJwt) {
+            return true;
+        }
+
+        const decoded = await this.authService.vertifyAsync(token);
+        if (!decoded) {
+            return false;
+        }
+
+        return requiredRoles.some((role) => decoded.role?.includes(role));
     }
-    const req = context.switchToHttp().getRequest() as Request;
-    const authorization = req.headers['authorization'];
-    if (!authorization) {
-      return false;
-    }
-
-    if (!authorization.startsWith('Bearer ')) {
-      console.log('Bearer로 시작하지 않는 토큰입니다');
-      return false;
-    }
-
-    // 토큰을 추출합니다.
-    const token = authorization.slice(7, authorization.length);
-
-    // 특정 API를 제외합니다.
-    const isIgnoreJwt = Reflect.getOwnMetadata(
-      'ignoreJwt',
-      context.getHandler(),
-    );
-
-    if (isIgnoreJwt) {
-      return true;
-    }
-
-    const decoded = await this.authService.vertifyAsync(token);
-    if (!decoded) {
-      return false;
-    }
-
-    return requiredRoles.some((role) => decoded.role?.includes(role));
-  }
 }
