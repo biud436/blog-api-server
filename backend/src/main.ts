1c4153c0a9bfa0c4097bfe1b92260adfae938cf5
biud436 <biud436@gmail.com>
2022-08-07 13:29:17 +0900

    fix: removed comments

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -106,7 +106,7 @@ export class NestBootstrapApplication {
                 secret: NestBootstrapApplication.CONFIG.get('APP_SECRET'),
                 resave: false,
                 saveUninitialized: false,
-                // store: new session.MemoryStore(), // redis store가 더 나아보임
+                // store: new session.MemoryStore(),
                 store: new RedisStore({ client: redisStoreMiddleware }),
                 // store: new MySQLStore({
                 //     host: NestBootstrapApplication.CONFIG.get('DB_HOST'),
