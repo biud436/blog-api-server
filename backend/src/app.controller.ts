a34ef25230e8a23104be7b228a35b9728b0af55d
biud436 <biud436@gmail.com>
2022-12-05 18:04:23 +0900

    fix: redirect as github when accessing main router

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,15 +3,22 @@ import {
     Get,
     InternalServerErrorException,
     Render,
+    Res,
 } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
     @Get()
     @ApiExcludeEndpoint()
-    getHello(): string {
-        return process.platform;
+    getHello(
+        @Res({
+            passthrough: true,
+        })
+        res: Response,
+    ): void {
+        res.redirect('https://github.com/biud436/blog-api-server');
     }
 
     @Render('login')
