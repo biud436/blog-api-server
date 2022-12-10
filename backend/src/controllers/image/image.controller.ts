2303df90e47f0796eff883d0374986e53026af9f
biud436 <biud436@gmail.com>
2022-12-11 00:01:07 +0900

    refactor: splited the html string as handlebar template engine

--- backend/src/controllers/image/image.controller.ts
+++ backend/src/controllers/image/image.controller.ts
@@ -45,6 +45,7 @@ import { RESPONSE_MESSAGE } from 'src/utils/response';
 import { IResponsableData } from 'src/utils/response.interface';
 import { S3ImageUploadDto } from './dto/s3-image-upload.dto';
 import { UserId } from 'src/decorators/x-api-key.decorator';
+import Handlebars from 'handlebars';
 
 @Controller('image')
 @ApiTags('이미지')
@@ -142,13 +143,14 @@ export class ImageController {
         },
         description: '깃허브 프로필용 이미지를 생성합니다.',
     })
+    @Render('svg-profile')
     async getStatsSvg(
         @Query('text') text: string,
         @Query('username') username: string,
         @Query('color') color: string,
         @Query('textSize', ParseIntPipe) textSize = 60,
         @Query('y', ParseIntPipe) y = 50,
-        @Res({ passthrough: true }) res: Response,
+        // @Res({ passthrough: true }) res: Response,
     ) {
         const { data } = (await this.httpService.axiosRef.get(
             `https://api.github.com/users/${username}`,
@@ -156,200 +158,24 @@ export class ImageController {
 
         const { login, name, public_repos, followers } = data;
 
-        return `
-        <svg
-            id='visual'
-            viewBox='0 0 900 300'
-            width='900'
-            height='300'
-            xmlns='http://www.w3.org/2000/svg'
-            xmlns:xlink='http://www.w3.org/1999/xlink'
-            version='1.1'
-            preserveAspectRatio="none"
-        >
-        <style>
+        const parameters = {
+            name: name ?? login,
+            followers: followers ?? 0,
+            date: new Date().toLocaleTimeString(),
+        };
 
-            .ping-pong {
-                animation: pong 1s ease-in-out infinite;
-                width: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-            }
-
-            .smooth-text {
-                font-size: 4.5em;
-                font-weight: bold;
-                font-family: 'Noto Sans KR', sans-serif;
-                text-align: center;
-                color: #0e0e0e;
-                animation: smooth-text 1s ease-in-out infinite;
-            }
-
-            .smooth-text-reverse {
-                font-size: 4.5em;
-                font-weight: bold;
-                font-family: 'Noto Sans KR', sans-serif;
-                text-align: center;
-                color: #0e0e0e;
-                animation: smooth-text-reverse 1s ease-in-out infinite;
-            }
-
-            date-text {
-                font-size: 1.5em;
-            }
-
-            @keyframes smooth-text {
-                0% {
-                    transform: translateX(0);
-                }
-
-                50% {
-                    transform: translateX(10px);
-                }
-
-                100% {
-                    transform: translateX(0);
-                }
-            }
-
-            @keyframes smooth-text-reverse {
-                0% {
-                    transform: translateX(0);
-                }
-
-                50% {
-                    transform: translateX(-10px);
-                }
-
-                100% {
-                    transform: translateX(0);
-                }
-            }
-
-            .ping-pong-2 {
-                animation: pong-1 1s ease-in-out infinite;
-                width: 100%;
-                display: flex;
-                justify-content: center;
-                align-items: center;                
-            }
-
-            @keyframes pong {
-                0% {
-                    fill: #fff;
-                    font-size: 1;
-                    transform: translate(0, 0);
-                }
-
-                50% {
-                    fill: #000;
-                    font-size: 1.4em;
-                    transform: translate(0, 2px);
-                }
-
-                100% {
-                    fill: #fff;
-                    font-size: 1;
-                    transform: translate(0, 0);
-                }
-            }
-
-            @keyframes pong-1 {
-                0% {
-                    fill: #fff;
-                    font-size: 1;
-                }
-
-                50% {
-                    fill: #000;
-                    font-size: 1.2em;
-                }
-
-                100% {
-                    fill: #fff;
-                    font-size: 1;
-                }
-            }
-
-            .colorize-bg {
-                animation: colorize 1s ease-in-out infinite;
-            }
-
-            @keyframes colorize {
-                0% {
-                    fill: #fff;
-                    text-shadow: 0 0 0 #fff;
-                }
-
-                100% {
-                    fill: #efefef;
-                    text-shadow: 0 0 0 #000;
-                }
-            }
-
-
-        </style>
-        
-        <rect width="900" height="300" fill="gray" class="colorize-bg">
-            <animate
-            attributeName="rx"
-            values="0;80;0"
-            dur="10s"
-            repeatCount="indefinite" />
-        </rect>
-
-        <text
-            x="50%"
-            y="30%"
-            text-anchor="middle"
-            font-size="4.5em"
-            font-weight="bold"
-            fill="#000"
-            class="smooth-text"
-        >${name ?? login}'s Profile</text>
-
-        ${text
-            .split('')
-            .map((char, index) => {
-                return `
-            <text
-                x="${10 + index * 20}"
-                y="${y}"
-                fill="#${color}"
-                font-size="${textSize}"
-                class="${index % 3 === 0 ? 'ping-pong' : 'ping-pong-2'}"
-            >
-                ${char}
-            </text>
-            `;
-            })
-            .join('')}  
-
-        <text
-            x="50%"
-            y="80%"
-            text-anchor="middle"
-            font-size="3.5em"
-            font-weight="bold"
-            fill="#000"
-            class="smooth-text-reverse"
-        >${followers} Followers</text>
-
-        <!-- Last Updated Text -->
-        <text
-            x="50%"
-            y="90%"
-            text-anchor="right"
-            fill="#000"
-            class="date-text"
-        >Last Updated: ${new Date().toLocaleString()}</text>
-    </svg>        
-    `
-            .replace('{{text}}', text)
-            .replace('{{color}}', color)
-            .replace('{{textSize}}', textSize.toString())
-            .replace('{{y}}', y.toString())
-            .replace('{{dy}}', (y + 50).toString());
+        return {
+            ...parameters,
+            texts: (() => {
+                return text.split('').map((char, index) => ({
+                    x: 10 + index * 20,
+                    y,
+                    color,
+                    textSize,
+                    className: index % 3 === 0 ? 'ping-pong' : 'ping-pong-2',
+                    content: char,
+                }));
+            })(),
+        };
     }
 }
