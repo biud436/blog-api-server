ad92fa67f0d2b10ed39526febc38d0a5e89032d8
biud436 <biud436@gmail.com>
2022-12-13 17:19:31 +0900

    comment: added a new comment

--- backend/src/modules/slack/slack.decorator.ts
+++ backend/src/modules/slack/slack.decorator.ts
@@ -2,8 +2,16 @@ import * as toss from '@toss/nestjs-aop';
 import { SlackLogger, SLACK_LOGGER_DECORATOR } from './slack.logger';
 
 /**
- * IOC 컨테이너에 의해 DI를 받아야 하기 때문에 SlackModule에 Provider로 등록해야 합니다.
- * decorators 폴더로 옮기지 마십시오.
+ * @toss.Aspect는 Injectable 데코레이터와 조합되어있기 때문에 Provider와 같습니다.
+ * Injectable 데코레이터를 포함하므로 IOC 컨테이너에 의해 DI를 받을 수 있습니다.
+ *
+ * 이름과 달리 Injetable하며 기능을 정의하기 때문에, 순수한 데코레이터와는 구분됩니다.
+ * 따라서 decorators 폴더로 옮기지 마시기 바랍니다.
+ *
+ * 클래스명은 튜토리얼에 근거하여 이름을 명명했으나 순수 데코레이터와 혼동을 줄 가능성이 있기 때문에,
+ * 적당한 이름으로 변경할 생각입니다.
+ *
+ * 이 클래스는 SlackModule에 Provider로 등록해야 합니다.
  */
 @toss.Aspect(SLACK_LOGGER_DECORATOR)
 export class SlackDecorator implements toss.LazyDecorator<any, any> {
