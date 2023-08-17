559eb20bb4726730481f523acbf0f608e7256870
biud436 <biud436@gmail.com>
2023-08-17 23:34:55 +0900

    chore: swc 오류로 webpack으로 복구

---
+++ backend/src/common/config/server-config.ts
@@ -0,0 +1,78 @@
+import { EventEmitter } from 'events';
+// import * as fs from 'fs';
+// import * as path from 'path';
+// import YAML from 'yaml';
+
+interface IServerConfig {
+    server: {
+        whitelist: string[];
+        host: {
+            local: string;
+            production: string[];
+        };
+        port: number;
+    };
+    redis: {
+        host: string;
+        port: number;
+    };
+}
+
+export class ServerConfig implements IServerConfig {
+    server: {
+        whitelist: string[];
+        host: {
+            local: string;
+            production: string[];
+        };
+        port: number;
+    };
+    redis: {
+        host: string;
+        port: number;
+    };
+
+    constructor({ server, redis }: IServerConfig) {
+        this.server = server;
+        this.redis = redis;
+    }
+}
+
+/**
+ * @class ServerConfigFactory
+ */
+export class ServerConfigFactory {
+    public static EVENT = new EventEmitter();
+
+    private readTargetFile(): string {
+        const target = process.env.NODE_ENV || 'development';
+
+        switch (target) {
+            default:
+            case 'development':
+                return '.config-local.yaml';
+            case 'production':
+                return '.config.yaml';
+        }
+    }
+
+    // async ready() {
+    //     try {
+    //         const content = await fs.promises.readFile(
+    //             path.join(__dirname, '..', '..', '..', this.readTargetFile()),
+    //             'utf-8',
+    //         );
+    //         const config = YAML.parse(content);
+
+    //         const serverConfig = new ServerConfig(config);
+
+    //         ServerConfigFactory.EVENT.emit('load', serverConfig);
+
+    //         return serverConfig;
+    //     } catch (e: any) {
+    //         throw new Error(
+    //             `설정 파일을 읽는 도중 오류가 발생했습니다. ${e.message}`,
+    //         );
+    //     }
+    // }
+}
