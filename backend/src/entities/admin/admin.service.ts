050ff71df2ccf87e4dfb7ea2f5d1442674c32a60
biud436 <biud436@gmail.com>
2023-09-26 23:48:40 +0900

    feat: added connection pool length

--- backend/src/entities/admin/admin.service.ts
+++ backend/src/entities/admin/admin.service.ts
@@ -1,14 +1,34 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { Admin } from './entities/admin.entity';
 
 @Injectable()
-export class AdminService {
+export class AdminService implements OnModuleInit {
     constructor(
         @InjectRepository(Admin)
         private readonly adminRepository: Repository<Admin>,
+        @InjectDataSource() private readonly dataSource: DataSource,
     ) {}
+
+    async onModuleInit() {
+        // MysqlDriver < Driver
+        const pool = (this.dataSource.driver as any).pool;
+        if ('_allConnections' in pool) {
+            const currentConnections = pool._allConnections;
+            console.log('연결 수: ', currentConnections.length);
+        }
+
+        // https://github.com/mysqljs/mysql/issues/1771
+        // pool?.on('connection', (connection: unknown) => {
+        //     console.log(connection);
+        //     if ('_allConnections' in pool) {
+        //         const currentConnections = pool._allConnections;
+
+        //         console.log('연결 수: ', currentConnections.length);
+        //     }
+        // });
+    }
 
     async isAdmin(username: string): Promise<boolean> {
         const count = await this.adminRepository.count({
