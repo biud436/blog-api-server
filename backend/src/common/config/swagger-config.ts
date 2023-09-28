be42b57e1b2ecb08b2e3d5ab72ab38b1067d30d6
biud436 <biud436@gmail.com>
2023-09-28 22:05:14 +0900

    fix: 회원 가입, 인증코드 전송/확인 기능, 토큰 재발급 기능 제거

--- backend/src/common/config/swagger-config.ts
+++ backend/src/common/config/swagger-config.ts
@@ -64,30 +64,6 @@ export namespace DocsMapper {
                 description: '로그인 성공시 토큰을 반환합니다.',
                 basicAuth: true,
             },
-            sendAuthCodeByEmail: {
-                operation: {
-                    summary: '이메일로 인증 코드 전송',
-                    description: '이메일로 인증 코드 6자리를 전송합니다.',
-                },
-                description: '이메일로 인증 코드 6자리를 전송합니다.',
-                auth: false,
-            },
-            verifyAuthCode: {
-                operation: {
-                    summary: '인증 코드 확인',
-                    description: '인증 코드를 확인합니다.',
-                },
-                description: '인증 코드를 확인합니다.',
-                auth: false,
-            },
-            signup: {
-                operation: {
-                    summary: '회원가입',
-                    description: '회원가입을 처리합니다.',
-                },
-                description: '회원가입을 처리합니다.',
-                auth: false,
-            },
         },
     };
 
