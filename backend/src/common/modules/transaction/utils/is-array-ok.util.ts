090fcd3f07a16afadb3e5b46344239bf996c3fbc
biud436 <biud436@gmail.com>
2023-10-12 16:22:12 +0900

    feat: added comment

--- backend/src/common/modules/transaction/utils/is-array-ok.util.ts
+++ backend/src/common/modules/transaction/utils/is-array-ok.util.ts
@@ -1,3 +1,9 @@
+/**
+ * 배열이 존재하고, 길이가 0보다 큰지 확인합니다.
+ *
+ * @param value
+ * @returns
+ */
 export function isArrayOK<T = any>(value: ArrayLike<any>): boolean {
     return Array.isArray(value) && value.length > 0;
 }
