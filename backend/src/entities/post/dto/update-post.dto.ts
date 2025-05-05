3fab29079e48932638f7a72c9407d6df77ffadc1
biud436 <biud436@gmail.com>
2025-05-05 17:49:10 +0900

    fix: Update UpdatePostDto to improve formatting and increase content maxLength to 10000

--- backend/src/entities/post/dto/update-post.dto.ts
+++ backend/src/entities/post/dto/update-post.dto.ts
@@ -5,34 +5,34 @@ import { Assert } from 'src/common/config/create-dto-common';
 import { CreatePostDto } from './create-post.dto';
 
 export class UpdatePostDto {
-    /**
-     * @title 글 제목
-     */
-    @ApiProperty()
-    @Assert.IsNotEmpty('제목을 입력해주세요.')
-    @IsString()
-    title!: string;
+  /**
+   * @title 글 제목
+   */
+  @ApiProperty()
+  @Assert.IsNotEmpty('제목을 입력해주세요.')
+  @IsString()
+  title!: string;
 
-    /**
-     * @title 글 내용
-     * @minLength 1
-     * @maxLength 4000
-     */
-    @ApiProperty()
-    @Assert.MinLength(1, {
-        message: '내용을 입력해주세요.',
-    })
-    @Assert.MaxLength(4000)
-    @IsString()
-    content!: string;
+  /**
+   * @title 글 내용
+   * @minLength 1
+   * @maxLength 4000
+   */
+  @ApiProperty()
+  @Assert.MinLength(1, {
+    message: '내용을 입력해주세요.',
+  })
+  @Assert.MaxLength(10000)
+  @IsString()
+  content!: string;
 
-    authorId?: number;
+  authorId?: number;
 
-    /**
-     * @title 대분류 (FK)
-     */
-    @ApiProperty()
-    @IsOptional()
-    @IsNumber()
-    categoryId?: number;
+  /**
+   * @title 대분류 (FK)
+   */
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  categoryId?: number;
 }
